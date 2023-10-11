ecaf2f9e5916f4b194a1bf0ecb0741ea25a33a9c
blais <blais@furius.ca>
2023-10-10 20:44:06 -0400

    Added constraints on python2 (thanks Stavros Ntentos).

--- setup.py
+++ setup.py
@@ -12,46 +12,60 @@ import sys
 
 
 # Install all scripts under bin.
-scripts = list(filter(isfile, [join('bin', x) for x in os.listdir('bin')]))
+scripts = list(filter(isfile, [join("bin", x) for x in os.listdir("bin")]))
+
 
 def read_version():
     try:
-        return open('VERSION', 'r').readline().strip()
+        return open("VERSION", "r").readline().strip()
     except IOError:
         _, e, _ = sys.exc_info()
         raise SystemExit(
-            "Error: you must run setup from the root directory (%s)" % str(e))
+            "Error: you must run setup from the root directory (%s)" % str(e)
+        )
 
 
 # Include all files without having to create MANIFEST.in
 def add_all_files(fun):
     import os, os.path
     from os.path import abspath, dirname, join
+
     def f(self):
-        for root, dirs, files in os.walk('.'):
-            if '.hg' in dirs: dirs.remove('.hg')
-            self.filelist.extend(join(root[2:], fn) for fn in files
-                                 if not fn.endswith('.pyc'))
+        for root, dirs, files in os.walk("."):
+            if ".hg" in dirs:
+                dirs.remove(".hg")
+            self.filelist.extend(
+                join(root[2:], fn) for fn in files if not fn.endswith(".pyc")
+            )
         return fun(self)
+
     return f
+
+
 from distutils.command.sdist import sdist
+
 sdist.add_defaults = add_all_files(sdist.add_defaults)
 
 
-setup(name="snakefood",
-      version=read_version(),
-      description=\
-      "Dependency Graphing for Python",
-      long_description="""
+setup(
+    name="snakefood",
+    version=read_version(),
+    description="Dependency Graphing for Python",
+    long_description="""
 Generate dependencies from Python code, filter, cluster and generate graphs
 from the dependency list.
 """,
-      license="GPL",
-      author="Martin Blais",
-      author_email="blais@furius.ca",
-      url="http://furius.ca/snakefood",
-      download_url="http://github.com/blais/snakefood",
-      package_dir = {'': 'lib/python'},
-      packages = ['snakefood', 'snakefood/fallback'],
-      scripts=scripts
-     )
+    license="GPL",
+    author="Martin Blais",
+    author_email="blais@furius.ca",
+    url="http://furius.ca/snakefood",
+    download_url="http://github.com/blais/snakefood",
+    package_dir={"": "lib/python"},
+    packages=["snakefood", "snakefood/fallback"],
+    scripts=scripts,
+    python_requires="<3",
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+    ],
+)
